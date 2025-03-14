83a45ac26f0088f1e9066286589df951003ceefc
Darryl <hjdjoo@gmail.com>
2025-03-14 16:42:43 -0400

    Continuing with nginx config and server route updates

--- src/server.ts
+++ src/server.ts
@@ -33,6 +33,9 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(SUPABASE_JWT_SECRET))
 
+app.use("/", (_req: Request, res: Response) => {
+  res.status(200).json("ok");
+})
 app.use("/health", (_req: Request, res: Response) => {
   res.status(200).json("ok");
 });
